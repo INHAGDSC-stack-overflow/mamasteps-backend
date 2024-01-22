8d245c719fc3cad69b7930420b53c851f7fda1b7
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-01-22 17:26:03 +0900

    feat: 스프링 부트 프로젝트 생성

---
+++ src/main/java/inhagdsc/mamasteps/MamastepsApplication.java
@@ -0,0 +1,13 @@
+package inhagdsc.mamasteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamastepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamastepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inhagdsc/mamasteps/MamastepsApplicationTests.java
@@ -0,0 +1,13 @@
+package inhagdsc.mamasteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamastepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
