91b62e649fa1ff82dc34e3d876db7ae91353fad3
Gyuhyeok <ghkdrbgur13@naver.com>
2024-01-23 11:37:53 +0900

    feat: UserDetails 추가

--- src/main/java/inhagdsc/mamasteps/user/entity/User.java
+++ src/main/java/inhagdsc/mamasteps/user/entity/User.java
@@ -9,6 +9,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Builder
 @NoArgsConstructor
@@ -16,11 +20,53 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @Table(name = "users")
-public class User extends BaseTimeEntity {
+public class User extends BaseTimeEntity implements UserDetails {
 
     @Id
     @GeneratedValue
     private Long id;
+
+    private String email;
+    private String password;
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return null;
+        }
+
+
+        @Override
+        public String getUsername() {
+            return email;
+        }
+        @Override
+        public String getPassword() {
+            return password;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
 
 }
 
