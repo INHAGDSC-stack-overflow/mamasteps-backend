5b495f373aeae12aff778587620f0ee497fc34d0
Gyuhyeok <ghkdrbgur13@naver.com>
2024-01-23 11:27:24 +0900

    refactor: User 임포트 수정

--- src/main/java/inhagdsc/mamasteps/user/repository/UserRepository.java
+++ src/main/java/inhagdsc/mamasteps/user/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package inhagdsc.mamasteps.user.repository;
 
 
+import inhagdsc.mamasteps.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
