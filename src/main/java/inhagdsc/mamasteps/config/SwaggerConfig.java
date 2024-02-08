ac2c8a15d92f774a6d3a3e4badb9843677c6b94b
Gyuhyeok <ghkdrbgur13@naver.com>
2024-02-08 22:33:02 +0900

    feat: 스웨거 https 설정 추가

--- src/main/java/inhagdsc/mamasteps/config/SwaggerConfig.java
+++ src/main/java/inhagdsc/mamasteps/config/SwaggerConfig.java
@@ -29,7 +29,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
     public OpenAPI OpenApi() {
 
         return new OpenAPI()
-                .addServersItem(new Server().url("/"))
+                .addServersItem(new Server().url("https://yourdomain.com"))
                 .components(new Components()
                         // 여기부터 추가 부분
                         .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
